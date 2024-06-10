a49d3655fcdd0e5fef30864bd452da06d4341967
greenmg0102 <greenmg0102@proton.me>
2024-06-10 15:33:15 -0700

    ###

--- src/api/user/opportunity/index.tsx
+++ src/api/user/opportunity/index.tsx
@@ -5,16 +5,18 @@ export async function opportunityRead(data: any): Promise<any> {
     return result
 }
 
-
 export async function opportunityFilerRead(): Promise<any> {
+    console.log('start ~~~');
+
     const result = await axios.get("/user/opportunity/filter-read").then((result: any) => { return result.data })
+    
+    console.log('end ~~~');
+    
     return result
 }
-
 
 
 export async function stemAccordingtoOpportunityRead(data: any): Promise<any> {
     const result = await axios.post("/user/opportunity/stem-accordingto-opportunity-read ", data).then((result: any) => { return result.data })
     return result
 }
-
--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -119,7 +119,7 @@ const Sidebar = () => {
                                 <NavLink to="/general-field-extract" className="group">
                                     <div className="flex items-center">
                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="unordered-list" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 10112 0 56 56 0 10-112 0zm0 284a56 56 0 10112 0 56 56 0 10-112 0zm0 284a56 56 0 10112 0 56 56 0 10-112 0z"></path></svg>
-                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">General field Extract</span>
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">Career Path Extract</span>
                                         {/* <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('chat')}</span> */}
                                     </div>
                                 </NavLink>
