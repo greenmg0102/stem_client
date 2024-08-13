5b2d21673bb6918bcd0559a23c3d2287d06217ad
Masaki Saito <masaki_saito9891@outlook.com>
2024-08-13 10:14:06 -0700

    $

--- src/components/UIs/SearchModule/SearchModuleItem.tsx
+++ src/components/UIs/SearchModule/SearchModuleItem.tsx
@@ -5,8 +5,9 @@ import SearchModuleISchoolList from './SearchModuleISchoolList'
 const SearchModuleItem = ({ isUnique, item, viewType }: any) => {
 
     return (
-        <div 
-        style={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}>
+        <div
+            style={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}
+        >
             {viewType === "list" && !isUnique ? <SearchModuleItemList item={item} /> : null}
             {viewType === "list" && isUnique ? <SearchModuleISchoolList item={item} /> : null}
         </div>
--- src/components/UIs/SearchModule/SearchModuleItemList.tsx
+++ src/components/UIs/SearchModule/SearchModuleItemList.tsx
@@ -7,13 +7,13 @@ const SearchModuleItemList = ({ item }: any) => {
 
     return (
         <div
-            className={clsx('relative w-full border  hover:border-gray-500 p-4 hover:shadow-xl cursor-pointer transition-all rounded-[4px]', isSelect ? "border-gray-500 shadow-lg" : "border-gray-200")}
+            className={clsx('relative w-full border border-red-500 hover:border-gray-500 p-4 hover:shadow-xl cursor-pointer transition-all rounded-[4px]', isSelect ? "border-gray-500 shadow-lg" : "border-gray-200")}
             // onClick={() => window.open(`/integration-search-id/${item._id}`, '_blank')}
 
             style={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}
         >
 
-            <div className="absolute top-[20px] right-[50px]" onClick={() => setIsSelect(!isSelect)}>
+            <div className="absolute bottom-[20px] right-[50px]" onClick={() => setIsSelect(!isSelect)}>
                 {!isSelect ?
                     <div className="flex justify-start items-center">
                         <svg className="text-blue-500" viewBox="64 64 896 896" focusable="false" data-icon="plus-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M696 480H544V328c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v152H328c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h152v152c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V544h152c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
